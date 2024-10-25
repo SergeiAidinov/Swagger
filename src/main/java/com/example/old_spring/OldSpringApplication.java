e8f0cfa7de6284785447abd8c894b590f8450bd4
Sergei Aidinov <incoming34@yandex.ru>
2024-10-25 15:51:20 +0300

    Initial commit

---
+++ src/main/java/com/example/old_spring/OldSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.old_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldSpringApplication.class, args);
+	}
+
+}
